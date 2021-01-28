bba8445adf6eb9e6f9dcef09d44c1f3552ecf483
Jeremy Stryer <jm.stryer@gmail.comgit config --global user.name Jeremy>
2021-01-27 21:13:45 -0600

    Create classes and first attempt at rendering article info

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let searchField = document.querySelector("#search-box");
+  class App {
+    constructor() {
+      // this.utilities = new Utilities();
+      // this.api = new API();
+      this.gallery = new Gallery(); 
+      this.init();
+    }
 
-  searchField.addEventListener("change", event => {
-    let query = event.target.value;
+    init() {
+      this.gallery.init();
+    }
+  }
 
-    fetch(`https://newscatcher.p.rapidapi.com/v1/search?q=${query}&sources=cnn.com&page_size=1`, {
+  class Utilities {
+    static getTodayDate() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0');
+      var yyyy = today.getFullYear();
+
+      return yyyy + '/' + mm + '/' + dd;
+    }
+  }
+  class Gallery {
+    init() {
+      let date = Utilities.getTodayDate();
+      API.getNews(null, date, this.populatePage);
+    }
+
+    populatePage(title, summary, link) {
+      let div1 = document.querySelector("#div-1");
+      div1.innerText = title;
+    }
+  }
+
+  class API {
+    static getNews(query, date, populate) {
+      if (!query) query = '*';
+
+       fetch(`https://newscatcher.p.rapidapi.com/v1/search?q=${query}&topic=news&sources=cnn.com&country=US&lang=en&from=${date}&page_size=1`, {
       "method": "GET",
       "headers": {
         "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
@@ -15,12 +49,44 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json();
       })
       .then(data => {
-        console.log(data);
+        let article = data.articles[0];
+        article.title;
+        article.summary;
+        article.link;
+        console.log(article);
+        populate(article.title);
       })
       .catch(err => {
         console.error(err);
       });
-  });
+    }
+  }
+
+  new App();
+});
+
+  
+
+  // searchField.addEventListener("change", event => {
+  //   let query = event.target.value;
+
+  //   fetch(`https://newscatcher.p.rapidapi.com/v1/search?q=${query}&sources=cnn.com&lang=en`, {
+  //     "method": "GET",
+  //     "headers": {
+  //       "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
+  //       "x-rapidapi-host": "newscatcher.p.rapidapi.com"
+  //     }
+  //     })
+  //     .then(response => {
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // });
 
 
 
@@ -41,71 +107,3 @@ document.addEventListener("DOMContentLoaded", () => {
   //   console.error(err);
   // });
 
-  // fetch("https://newscatcher.p.rapidapi.com/v1/search_free?q=Trump&lang=en&sort_by=date&sources=nytimes.com&page_size=1", {
-  //   "method": "GET",
-  //   "headers": {
-  //     "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
-  //     "x-rapidapi-host": "newscatcher.p.rapidapi.com"
-  //   }
-  //   })
-  //   .then(response => {
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //   });
-
-  // fetch("https://newscatcher.p.rapidapi.com/v1/search_free?q=Trump&sort_by=date&sources=wsj.com&page_size=1", {
-  //   "method": "GET",
-  //   "headers": {
-  //     "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
-  //     "x-rapidapi-host": "newscatcher.p.rapidapi.com"
-  //   }
-  //   })
-  //   .then(response => {
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //   });
-
-  // fetch("https://newscatcher.p.rapidapi.com/v1/search_free?q=Trump&sort_by=date&sources=foxnews.com&page_size=1", {
-  //   "method": "GET",
-  //   "headers": {
-  //     "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
-  //     "x-rapidapi-host": "newscatcher.p.rapidapi.com"
-  //   }
-  //   })
-  //   .then(response => {
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //   });
-
-  // fetch("https://newscatcher.p.rapidapi.com/v1/search_free?q=Trump&sort_by=date&sources=nationalreview.com&page_size=1", {
-  //   "method": "GET",
-  //   "headers": {
-  //     "x-rapidapi-key": "4a434644b7mshf0c157810805032p180f52jsn6f7ab924b303",
-  //     "x-rapidapi-host": "newscatcher.p.rapidapi.com"
-  //   }
-  //   })
-  //   .then(response => {
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //   });
-});
